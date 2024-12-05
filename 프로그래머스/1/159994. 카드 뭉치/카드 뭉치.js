c94906ff6eedbb30ecee265ba0dfcd6d5c45e81a
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-05 14:54:56 +0900

    [level 1] Title: 카드 뭉치, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,12 +1,11 @@
+
 function solution(cards1, cards2, goal) {
-    for(const x of goal){
-        if(cards1[0] === x){
-            cards1.shift()
-        }else if(cards2[0] === x){
-            cards2.shift()
-        }else {
-            return "No"
-        }
+    let j = 0;
+    let k = 0;
+    for(let i=0;i<goal.length;i++){
+        if(goal[i] == cards1[j]) j++;
+        else if(goal[i] == cards2[k]) k++;
+        else return "No"
     }
-    return "Yes";
+    return "Yes"; 
 }
