a1b7a1a7ea327bbf3c5f6ca7178c571dd2139a3b
Saem <104006855+alicelee1231@users.noreply.github.com>
2024-12-11 10:19:17 +0900

    [level 1] Title: 소수 찾기, Time: 50.40 ms, Memory: 45.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    if (n < 2) return []; 
+
+    let isPrime = Array(n + 1).fill(true);
+    isPrime[0] = isPrime[1] = false; 
+
+    for (let i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    let primes = [];
+    for (let i = 2; i <= n; i++) {
+        if (isPrime[i]) {
+            primes.push(i); 
+        }
+    }
+    return primes.length; 
+}
